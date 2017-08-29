746659c63e9f3efa88f7a577b7c0c4546f767419
Eafy <qyf@deepdata.com.cn>
2017-08-29 14:29:43 +0800

    iOS is OK

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-import React, { NativeModules } from 'react-native';
-module.exports = NativeModules.RNMarketCommentModule;
+import {
+  Linking,
+  Platform,
+  NativeModules
+} from 'react-native';
+
+const RNMarketCommentModule = NativeModules.RNMarketCommentModule;
+export const APP_STORE_COMMENT = 'itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=';
+
+export default function openAppStoreToComment (appId = 1210643082) {
+  if (Platform.OS === 'ios') {
+    const url = APP_STORE_COMMENT + appId;
+    Linking.openURL(url).catch(err => console.error('An error occurred', err));
+    return;
+  }
+  if (Platform.OS === 'android') {
+    RNMarketCommentModule.show();
+  }
+}
